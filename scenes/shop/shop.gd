78b2b3850769e7422a9cdafef896cb2469bbfb5d
yopox <mail@yopox.fr>
2024-09-18 18:32:29 +0200

    [shop] buying towers

--- scenes/shop/shop.gd
+++ scenes/shop/shop.gd
@@ -1,15 +1,16 @@
 class_name Shop extends Node2D
 
-@onready var level = $Header/Level
-@onready var coins = $Header/Coins
+@onready var level_label = $Header/Level
+@onready var coins_label = $Header/Coins
 @onready var slots = $Content/Slots
 @onready var board: Board = $Board
 @onready var cursor: Node2D = $Cursor
-@onready var status: Label = $Status
+@onready var status_label: Label = $Status
 
 enum State { Select, Move, Buy, SelectColumn, SelectRow }
 
 var state: State = State.Select
+var coins: int = 3
 var focused: Array[int] = [0, 1]
 var selected_slot: Slot = null
 
@@ -23,6 +24,7 @@ func _ready():
 	slots.get_child(3).locked = Progress.shop_r_locked
 	update_cursor()
 	update_status()
+	update_header()
 	reroll()
 
 
@@ -49,6 +51,8 @@ func _process(_delta):
 	if Input.is_action_just_pressed("a"):
 		a()
 		update_status()
+	elif Input.is_action_just_pressed("b"):
+		b()
 
 
 func a() -> void:
@@ -61,11 +65,21 @@ func a() -> void:
 		if focused[0] == 0: fight()
 		if focused[0] == 1: collection()
 		return
-	
-	if focused[1] in [2, 3]:
-		var slot: Slot = hovered_slot()
 		
-		if state == State.Select and slot.tower_node.tower != null and not slot.locked:
+	var slot: Slot = hovered_slot()
+	if focused[1] == 1:
+		if state == State.Select and not slot.locked and slot.tower_node.tower != null:
+			var cost: int = FightUtil.tower_level(slot.tower_node.tower.type)
+			if coins < cost:
+				# TODO: Not enough coins animation
+				pass
+			else:
+				state = State.Buy
+				selected_slot = slot
+			return
+
+	if focused[1] in [2, 3]:
+		if state == State.Select and not slot.locked and slot.tower_node.tower != null:
 			selected_slot = slot
 			state = State.Move
 		elif state == State.Move and not slot.locked:
@@ -76,7 +90,32 @@ func a() -> void:
 			selected_slot = null
 			state = State.Select
 			update_slots()
+		elif state == State.Buy and not slot.locked and slot.tower_node.tower == null:
+			# Actually buy the tower
+			var cost: int = FightUtil.tower_level(selected_slot.tower_node.tower.type)
+			coins -= cost
+			update_header()
+			# Place the tower
+			var t1: Variant = selected_slot.tower_node.tower
+			slot.set_tower(t1, 0)
+			# TODO: On build effect
+			selected_slot.set_tower(null, 0)
+			selected_slot = null
+			state = State.Select
+			update_slots()
+		return
 				
+
+func b() -> void:
+	match state:
+		State.Move, State.Buy:
+			selected_slot = null
+			state = State.Select
+			update_slots()
+			update_status()
+		State.Select: return
+		_: return
+	
 
 func hovered_slot() -> Slot:
 	if focused[1] == 1:
@@ -111,31 +150,42 @@ func update_slot(slot: Slot, dy: int) -> void:
 	slot.tower_node.show_popup(active, false)
 
 
+func update_header() -> void:
+	level_label.text = "Level %s" % Progress.shop_level
+	coins_label.text = "%s¢" % coins
+
+
 func update_status() -> void:
 	var slot: Slot = hovered_slot()
 	
 	if slot != null and slot.locked:
-		status.text = "Locked"
+		status_label.text = "Locked"
+		return
+	elif state == State.Buy:
+		status_label.text = "Select a slot"
 		return
 	elif slot != null and slot.tower_node.tower == null:
-		status.text = "Empty"
+		status_label.text = "Empty"
 		return
 	elif slot != null and focused[1] == 1:
-		status.text = "Buy for %s¢" % FightUtil.tower_level(slot.tower_node.tower.type)
+		status_label.text = "Buy for %s¢" % FightUtil.tower_level(slot.tower_node.tower.type)
+		return
+	elif slot != null and focused[1] in [2, 3]:
+		status_label.text = "%s" % Text.tower_name(slot.tower_node.tower.type)
 		return
 	
 	if state == State.Move:
-		status.text = "Move to?"
+		status_label.text = "Move to?"
 		return
 	
-	status.text = "-"
+	status_label.text = "-"
 	
 	if focused[1] == 0:
-		if focused[0] == 0: status.text = "Reroll (%s¢)" % Values.REROLL_COST
-		elif focused[0] == 1: status.text = "Upgrade (%s¢)" % Values.UPGRADE_COST
+		if focused[0] == 0: status_label.text = "Reroll (%s¢)" % Values.REROLL_COST
+		elif focused[0] == 1: status_label.text = "Upgrade (%s¢)" % Values.UPGRADE_COST
 	elif focused[1] == 4:
-		if focused[0] == 0: status.text = "Start the fight"
-		elif focused[0] == 1: status.text = "View all creatures"
+		if focused[0] == 0: status_label.text = "Start the fight"
+		elif focused[0] == 1: status_label.text = "View all creatures"
 
 
 func reroll() -> void:

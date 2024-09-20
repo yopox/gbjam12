4db92c1f7e7cf7b606d44c8683845fe9b840b836
yopox <mail@yopox.fr>
2024-09-20 21:31:00 +0200

    [data] update fighter data

--- autoload/fighter_data.gd
+++ autoload/fighter_data.gd
@@ -1,13 +1,24 @@
 extends Node
 
-var fighter: Array[Variant] = ["QAkAgAA=",
-							  "YAkAgATAIAA=",
-							  "ZAkAgATAIAEwCAA=",
-							  "ZgkAgATAIAEwCAEEAIA=",
-							  "9gkAgAAAEAQAAgBMAgATAIAwYDAA",
-							  "9gkAgAAAEAQAAgBMAgATAIAwYDAA"]
+var fighter: Array[Variant] = ["QJEAgAA=",
+							  "YJMAQCaAIAA=",
+							  "YpCAwCaAIACQCAA=",
+							  "YpKAIAJAIAmgCAA=",
+							  "5tMAwCRgKACQCADoAoCaAIAA",
+							  "9tIAwA6AKAmgCANkAICTgGACQCAA",
+							  "/9IAwA6AKAmgCAAkAgBBACAkEDgNkAICggCA",
+							  "/9CAwAJAIAOgCgNkAIBBACAIIDAKCAICSQBA",
+							  "/9IAwAbAEAOgCgNkAIBBACAIYCgKCAICRQJA",
+							  "/9IAwAbAEAOgCgNkAIBBACAIEDgKCAICTQFA",
+							  "/9IAwAbAEAOgCgNkAIBBACAIUCQKCAICQwNA",
+							  "/0VAgAbAEAOgCgNkAIBBACAIMDQKCAICSwDA",
+							  "/0VAgAbAEAOgCgNkAIBBACAIcCwKCAICRwLA",
+							  "/0VAgAbAEAOgCgNkAIBBACAICDwKCAICTwHA",
+							  "/0VAgAbAEAOgCgNkAIBBACAISCIKCAICQIPA",]
+
+
 
 
 func get_enemy_board(turn: int) -> Dictionary:
-	var b = fighter[5] if turn > 5 else fighter[turn - 1]
+	var b = fighter[14] if turn > 14 else fighter[turn - 1]
 	return Progress.import_board(b)

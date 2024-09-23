51f4a10eefc7905c034fcd463971038bc9b1bec1
yopox <mail@yopox.fr>
2024-09-23 08:56:22 +0200

    [fight] update speed

--- autoload/values.gd
+++ autoload/values.gd
@@ -24,17 +24,17 @@ var LEVEL_4_RANGES: Array[Array] = [range(0, 10), range(10, 30), range(30, 75), 
 #endregion
 
 #region Fight - General
-const FIGHT_REVEAL_DELAY: float = 1.3
+const FIGHT_REVEAL_DELAY: float = 1.2
 const FIGHT_DESTROY_BULLETS_DELAY: float = 0
 const FIGHT_START_DELAY: float = 1
-const FIGHT_END_DELAY: float = 0.75
+const FIGHT_END_DELAY: float = 1.0
 const FIGHT_TURN_DELAY: float = 2.25
 const VIBRATION_DURATION: int = 150
 const GAME_OVER_DELAY: float = 2
 #endregion
 
 #region Bullets
-const BULLET_SPEED: int = 60
+const BULLET_SPEED: int = 80
 const ARROW_SPEED: int = 160
 #endregion
 

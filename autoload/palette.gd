6b1933802887fa57d0b4e28ca953cf15299b2647
yopox <mail@yopox.fr>
2024-09-23 06:51:14 +0200

    [misc] add splash screen

--- autoload/palette.gd
+++ autoload/palette.gd
@@ -1,6 +1,6 @@
 extends Node
 
-enum Name { NEXUS_2060, YOPOX, GRAYSCALE }
+enum Name { NEXUS_2060, YOPOX, GREEN, GRAYSCALE }
 
 const WHITE = Color("ffffff")
 const LIGHT_GRAY = Color("aaaaaa")
@@ -19,5 +19,7 @@ func get_palette(p: Name) -> Array:
 			return [Color("201e33"), Color("ab1a5d"), Color("ffbc43"), Color("ffffff")]
 		Name.NEXUS_2060:
 			return [Color("2a110c"), Color("f1461b"), Color("faad1f"), Color("fdfdfd")]
+		Name.GREEN:
+			return [Color("545e7f"), Color("71ae8c"), Color("d1ea9f"), Color("ffffff")]
 		_:
 			return [Color("000000"), Color("555555"), Color("aaaaaa"), Color("ffffff")]
